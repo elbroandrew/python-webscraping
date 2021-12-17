9eea95b7c892cab2bcffb17b794f208514b5f3ba
Andrew <elbroandrew@gmail.com>
2021-12-17 16:55:00 +1000

    feat: find results

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+website = 'https://www.cars.com/shopping/results/?stock_type=cpo&makes%5B%5D=mercedes_benz&models%5B%5D=&list_price_max=&maximum_distance=20&zip='
+response = requests.get(website)
 
 
+#check the status code:
+print(response.status_code)
 
-
-
+soup = BeautifulSoup(response.content, 'html.parser')
+results = soup.find_all('div', {'class': 'vehicle-card'})
 
 
 def main():
-    pass
+    print(len(results))
 
 
 if __name__ == '__main__':
